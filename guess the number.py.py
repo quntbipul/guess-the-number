c121a59f9968e13f58df92bb64d9dc77994f592d
bipul gaur <147261190+quntbipul@users.noreply.github.com>
2023-12-01 01:43:45 -0800

    Add files via upload

---
+++ guess the number.py.py
@@ -0,0 +1,16 @@
+def madlib():
+    time_of_day =input("time of thr day : ")
+    body_of_plurlal = input("body part (plural) ")
+    color =input("color : ")
+    verb_past_tense =input("verb (past tense): ")
+    food = input("Food :")
+    noun1 =input("noun:")
+    noun_plural_1 = input("Noun(plural) : ")
+    adj1 = input("adjective : ")
+    adj2 = input("adjective : ")
+    adj3 = input("adjective : ")
+    madlib =f"it was a {adj1} summer {time_of_day} when we realized that the vaccine to stop \ spred of disease did not work. insted , it produced ZOMBIES!! they were thirsty for {body_part_plural}\ "
+               "and the streets were lined with these monsters holding {noun_plural_1} in thier hands \ "
+               "Thier eyes were {color} with hunger as they  {verb_past_tense} around the city looking for {food}.  \ they were {adj2} and {adj3} , unkonwing how to act in this human world ... expect eat {body_part_plural}!!!!"
+               "that was thier sole mission and they were  dedicated  towards achieving it for the sake of {noun1}. "
+             print(madlib)
